0ee4d75fb250e6880eae15edcc0bc95fc12e4de7
Jebakani <vjishwarya@gmail.com>
2021-07-07 21:01:49 +0530

    [JEBAKANI] Printing Arrays using Generic class

--- PrintGenericArray/PrintArray.cs
+++ PrintGenericArray/PrintArray.cs
@@ -6,27 +6,22 @@ using System.Threading.Tasks;
 
 namespace PrintGenericArray
 {
-    class PrintArray
+    class PrintArray<T>
     {
+        public T[] inputArray;
+        public PrintArray(T[] inputArray)
+        {
+            this.inputArray = inputArray;
+        }
         //Printing the array using generic type T
-        public static void ToPrintArray<T>(T[] inputArray)
+        public  void ToPrintArray()
         {
             Console.WriteLine("======Printing Array======");
-            foreach(var i in inputArray)
+            foreach(var i in this.inputArray)
             {
                 Console.WriteLine(i);
             }
         }
 
-      
-        public static void ReadInput()
-        {
-            int[] intArray = { 10, 5, 100, 85, 96, 2, 1 };
-            double[] doubleArray = { 1.1, 1.3, 5.6, 5.36, 6.2, 80.5, 1 };
-            char[] charArray = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
-            ToPrintArray(intArray);
-            ToPrintArray(doubleArray);
-            ToPrintArray(charArray);
-        }
     }
 }
--- PrintGenericArray/Program.cs
+++ PrintGenericArray/Program.cs
@@ -7,12 +7,18 @@ namespace PrintGenericArray
         /// <summary>
         /// UC1- Printing different array without generic type
         /// UC2- Printing array using generic type method
+        /// UC3- Printig Array using generic class
         /// </summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Printing array of different data type");
-            PrintArray.ReadInput();
+            int[] intArray = { 10, 5, 100, 85, 96, 2, 1 };
+            double[] doubleArray = { 1.1, 1.3, 5.6, 5.36, 6.2, 80.5, 1 };
+            char[] charArray = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+            new PrintArray<int>(intArray).ToPrintArray();
+            new PrintArray<double>(doubleArray).ToPrintArray();
+            new PrintArray<char>(charArray).ToPrintArray();
             Console.Read();
         }
     }
