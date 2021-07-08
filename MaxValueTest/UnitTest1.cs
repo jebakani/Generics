f1b86b3b378f28148b8e76552104599c7c8bf001
Jebakani <vjishwarya@gmail.com>
2021-07-08 20:17:49 +0530

    [JEBAKANI] Finding maximum of given numbers

--- MaxValueTest/UnitTest1.cs
+++ MaxValueTest/UnitTest1.cs
@@ -11,92 +11,27 @@ namespace MaxValueTest
         public void TestMethod1()
         {
 
-            int first = 80;
-            int second = 56;
-            int third = 21;
-            int actual = new MaxOfThree<int>(first, second, third).MaxMethod();
-            int expected = first;
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void TestMethod2()
-        {
-            int first = 70;
-            int second = 90;
-            int third = 21;
-            int actual = new MaxOfThree<int>(first, second, third).MaxMethod();
-            int expected = second;
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void TestMethod3()
-        {
-            int first = 80;
-            int second = 56;
-            int third = 100;
-            int actual = new MaxOfThree<int>(first, second, third).MaxMethod();
-            int expected = third;
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void TestMethodFloat1()
-        {
-            double first = 11.3;
-            double second = 5.6;
-            double third = 2.1;
-            double actual = new MaxOfThree<double>(first, second, third).MaxMethod();
-            double expected = first;
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void TestMethodFloat2()
-        {
-            double first = 8.0;
-            double second = 53.6;
-            double third = 2.1;
-            double actual = new MaxOfThree<double>(first, second, third).MaxMethod();
-            double expected = second;
-            Assert.AreEqual(expected, actual);
-        }
-        [TestMethod]
-        public void TestMethodFloat3()
-        {
-            double first = 8.0;
-            double second = 5.6;
-            double third = 12.1;
-            double actual = new MaxOfThree<double>(first, second, third).MaxMethod();
-            double expected = first;
+            int[] array= { 10, 5, 100, 85, 96, 2, 1 };
+            int actual = new MaxOfThree<int>(array).MaxMethod();
+            int expected = 100;
             Assert.AreEqual(expected, actual);
         }
 
         [TestMethod]
-        public void TestMethodString1()
+        public void TestMethodFloat1()
         {
-            string first = "Apple";
-            string second = "Peach";
-            string third = "Banana";
-            string actual = new MaxOfThree<string>(first, second, third).MaxMethod(); 
-            string expected = first;
+            double[] array = { 1.1, 1.3, 5.6, 5.36, 6.2, 80.5, 1 };
+            double actual = new MaxOfThree<double>(array).MaxMethod();
+            double expected = 80.5;
             Assert.AreEqual(expected, actual);
         }
-        [TestMethod]
-        public void TestMethodString2()
-        {
-            string first = "Peach";
-            string second = "Apple";
-            string third = "Banana";
-            string actual = new MaxOfThree<string>(first, second, third).MaxMethod();
-            string expected = first;
-            Assert.AreEqual(expected, actual);
-        }
+       
         [TestMethod]
         public void TestMethodString3()
         {
-            string first = "Banana";
-            string second = "Peach";
-            string third = "Apple";
-            string actual = new MaxOfThree<string>(first, second, third).MaxMethod();
-            string expected = second;
+            char[] first = { 'A', 'B', 'C', 'D', 'E', 'F', 'G' };
+            char actual = new MaxOfThree<char>(first).MaxMethod();
+            char expected = 'G';
             Assert.AreEqual(expected, actual);
         }
 
